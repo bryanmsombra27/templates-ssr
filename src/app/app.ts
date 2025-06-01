4a341e1e92c00610aa2d80f18a3c3acfead3a911
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-01 17:14:45 -0600

    template finished

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { Articles } from './components/articles/articles';
+import { NewsLetter } from './components/news-letter/news-letter';
+import { Cards } from './components/cards/cards';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Hero, Articles, NewsLetter, Cards],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'templates';
