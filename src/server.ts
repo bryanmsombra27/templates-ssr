f45f8e6961986b17d655af90cb5765283c045041
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-06 12:30:32 -0600

    adding robots.txt config file and compilation

--- src/server.ts
+++ src/server.ts
@@ -24,6 +24,10 @@ const angularApp = new AngularNodeAppEngine();
  * ```
  */
 
+app.get('/robots.txt', (req, res) => {
+  res.sendFile(join(browserDistFolder, 'robots.txt'));
+});
+
 /**
  * Serve static files from /browser
  */
@@ -32,7 +36,7 @@ app.use(
     maxAge: '1y',
     index: false,
     redirect: false,
-  }),
+  })
 );
 
 /**
@@ -42,7 +46,7 @@ app.use((req, res, next) => {
   angularApp
     .handle(req)
     .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
+      response ? writeResponseToNodeResponse(response, res) : next()
     )
     .catch(next);
 });
