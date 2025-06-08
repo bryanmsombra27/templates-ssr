f1ccd90d85a8c6304bad2e2f10f8a11ae2ce8090
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-08 12:17:37 -0600

    adding newsletter section to home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Cards } from '../../components/home/cards/cards';
 import { Newest } from '../../components/home/newest/newest';
+import { Newsletter } from '../../components/home/newsletter/newsletter';
 
 @Component({
   selector: 'app-home',
-  imports: [Cards, Newest],
+  imports: [Cards, Newest, Newsletter],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
