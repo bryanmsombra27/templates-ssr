e64e62306b5fde8f96e25e0c2695e3b697911bc1
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-08 13:12:06 -0600

    home page finished

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/shared/navbar/navbar';
+import { Footer } from './components/shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class App {
   protected title = 'templates';
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Cards } from '../../components/home/cards/cards';
 import { Newest } from '../../components/home/newest/newest';
 import { Newsletter } from '../../components/home/newsletter/newsletter';
+import { ContactInfo } from '../../components/home/contact-info/contact-info';
 
 @Component({
   selector: 'app-home',
-  imports: [Cards, Newest, Newsletter],
+  imports: [Cards, Newest, Newsletter, ContactInfo],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
