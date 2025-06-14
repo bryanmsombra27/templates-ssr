56c87d4e4e05e014a9b90898b06fbe7d47dc6a8a
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-14 14:31:37 -0600

    adding info card section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InfoCards } from "./components/info-cards/info-cards";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InfoCards],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
