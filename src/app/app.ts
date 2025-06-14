1ca7b84201a1e2e9323a167e3db8c5725ce92023
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-14 15:02:58 -0600

    project almost finished

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { InfoCards } from "./components/info-cards/info-cards";
+import { InfoCards } from './components/info-cards/info-cards';
+import { OrdersStatus } from './components/orders-status/orders-status';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InfoCards],
+  imports: [RouterOutlet, InfoCards, OrdersStatus],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'templates';
---
+++ src/app/components/orders-status/orders-status.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-status',
+  imports: [],
+  templateUrl: './orders-status.html',
+  styleUrl: './orders-status.css'
+})
+export class OrdersStatus {
+
+}
