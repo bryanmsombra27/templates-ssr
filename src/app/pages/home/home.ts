c840dbe97c39e6d10b7206f580693d5869deb92f
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-14 19:44:54 -0600

    home finished

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FirstSection } from '../../components/home/first-section/first-section';
 import { SecondSection } from '../../components/home/second-section/second-section';
+import { ThirdSection } from '../../components/home/third-section/third-section';
 
 @Component({
   selector: 'app-home',
-  imports: [FirstSection, SecondSection],
+  imports: [FirstSection, SecondSection, ThirdSection],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
