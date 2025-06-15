2a452a431cc10a363a8557813fef1ee10c95eb3e
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-14 19:07:31 -0600

    adding second section

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FirstSection } from '../../components/home/first-section/first-section';
+import { SecondSection } from '../../components/home/second-section/second-section';
 
 @Component({
   selector: 'app-home',
-  imports: [FirstSection],
+  imports: [FirstSection, SecondSection],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
