c58e3fa0730c911fb3bec7904233dba0847b993c
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-25 15:33:56 -0600

    adding soccer field section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
+import { DescriptionCards } from './components/description-cards/description-cards';
+import { Schedule } from './components/schedule/schedule';
+import { SoccerFields } from './components/soccer-fields/soccer-fields';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [
+    RouterOutlet,
+    Header,
+    Hero,
+    DescriptionCards,
+    Schedule,
+    SoccerFields,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
