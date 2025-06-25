e8b358894d89c8240f4c15adb8af2d37d2f482b6
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-25 17:51:18 -0600

    proyect finished

--- src/app/components/schedule/schedule.ts
+++ src/app/components/schedule/schedule.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  signal,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-schedule',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './schedule.html',
-  styleUrl: './schedule.css'
+  styleUrl: './schedule.css',
+  encapsulation: ViewEncapsulation.None,
 })
-export class Schedule {
+export class Schedule implements OnInit {
+  date = signal('');
+  @ViewChild('datepicker')
+  datePicker!: ElementRef;
 
+  isOpen = signal(false);
+
+  ngOnInit(): void {
+    this.formatDate(new Date());
+  }
+
+  formatDate(date: Date) {
+    const formater = new Intl.DateTimeFormat('es-ES', { dateStyle: 'full' });
+
+    this.date.set(formater.format(date));
+  }
+
+  setSelectedDate(e: any) {
+    const value = e.target.value;
+
+    this.formatDate(new Date(`${value}T12:00:00`));
+  }
+
+  toggleModal() {
+    this.isOpen.update((state) => !state);
+  }
 }
--- src/app/components/soccer-fields/soccer-fields.ts
+++ src/app/components/soccer-fields/soccer-fields.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Schedule } from '../schedule/schedule';
 
 @Component({
   selector: 'app-soccer-fields',
-  imports: [],
+  imports: [Schedule],
   templateUrl: './soccer-fields.html',
-  styleUrl: './soccer-fields.css'
+  styleUrl: './soccer-fields.css',
 })
 export class SoccerFields {
+  @ViewChild(Schedule)
+  schedule!: Schedule;
 
+  openModal() {
+    this.schedule.toggleModal();
+  }
 }
