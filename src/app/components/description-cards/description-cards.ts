0c72253e083cd7b6a478eee6cc0fcc5f4303c043
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-25 10:25:10 -0600

    adding header and hero section

---
+++ src/app/components/description-cards/description-cards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-description-cards',
+  imports: [],
+  templateUrl: './description-cards.html',
+  styleUrl: './description-cards.css'
+})
+export class DescriptionCards {
+
+}
---
+++ src/app/components/soccer-fields/soccer-fields.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soccer-fields',
+  imports: [],
+  templateUrl: './soccer-fields.html',
+  styleUrl: './soccer-fields.css'
+})
+export class SoccerFields {
+
+}
