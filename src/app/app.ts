795aa5dd117a781d741d9cfa80505a8f629f4efb
bryanmsombra27 <bryanjm96@gmail.com>
2025-08-07 13:57:35 -0600

    working on responsive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InfoCards } from './components/info-cards/info-cards';
 import { OrdersStatus } from './components/orders-status/orders-status';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InfoCards, OrdersStatus],
+  imports: [RouterOutlet, InfoCards, OrdersStatus, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
+  isActiveMenu = signal(false);
+
   protected title = 'templates';
+
+  toggleMenu() {
+    this.isActiveMenu.update((state) => !state);
+  }
 }
