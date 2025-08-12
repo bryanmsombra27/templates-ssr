761e8d4d9a2a5aac1692dac0fa10ae78813255b8
bryanmsombra27 <bryanjm96@gmail.com>
2025-08-12 12:19:51 -0600

    working in responsive

---
+++ src/app/components/shared/custom-container/custom-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-container',
+  imports: [],
+  templateUrl: './custom-container.html',
+  styleUrl: './custom-container.css'
+})
+export class CustomContainer {
+
+}
